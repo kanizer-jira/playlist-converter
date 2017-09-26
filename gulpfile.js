1b99067fbd1c6058b4ea981c0dba8d89ca34c229
Nelson Shin <nelson_shin@apple.com>
2017-09-26 14:22:51 -0700

    add new npm script to distinguish for electron

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,7 @@ gulp.task('build', gulp.series(gulp.parallel('other', 'webpack:dist')));
 gulp.task('test', gulp.series('karma:single-run'));
 gulp.task('test:auto', gulp.series('karma:auto-run'));
 gulp.task('serve', gulp.series('webpack:watch', 'watch', 'browsersync'));
+gulp.task('serve:web', gulp.series('webpack:web:watch', 'watch', 'browsersync'));
 gulp.task('serve:dist', gulp.series('default', 'browsersync:dist'));
 gulp.task('default', gulp.series('clean', 'build'));
 gulp.task('watch', watch);
